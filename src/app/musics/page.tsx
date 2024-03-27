f1b1257ced09a0125672302316815450a92520b2
Nguyen Cong Minh <minhnc@deha-soft.com>
2024-03-27 23:50:41 +0700

    add react player npm & add route

--- src/app/musics/page.tsx
+++ src/app/musics/page.tsx
@@ -1,9 +1,15 @@
+"use client";
 import React from "react";
+import ReactPlayer from "react-player";
 
 type Props = {};
 
 const MusicList = (props: Props) => {
-  return <div>ListMusic</div>;
+  return (
+    <div className="flex justify-center align-center">
+      <ReactPlayer url="https://www.youtube.com/watch?v=uDgBO6brbSI" />
+    </div>
+  );
 };
 
 export default MusicList;

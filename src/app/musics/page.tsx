1e5816a353346a10a89e9c25c69b52f2d841de3a
Nguyen Cong Minh <minhnc@deha-soft.com>
2024-04-12 23:43:21 +0700

    change font & add side bar

--- src/app/musics/page.tsx
+++ src/app/musics/page.tsx
@@ -8,11 +8,11 @@ const PlayerFrameDynamic = dynamic(() => import("@/components/PlayerFrame"), { s
 
 const MusicList = (props: Props) => {
   return (
-    <div className="flex flex-col justify-between">
-      <div className="flex justify-center align-center">
+    <div className="flex flex-col justify-between align-center min-h-screen">
+      <div className="flex justify-center align-center flex-1">
         <ReactPlayerYoutube url="https://www.youtube.com/watch?v=uDgBO6brbSI" />
       </div>
-      <section>
+      <section className="flex justify-center align-center min-h-5">
         <PlayerFrameDynamic />
       </section>
     </div>
